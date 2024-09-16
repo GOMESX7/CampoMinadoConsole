e15fc9ac73c3613262a4f77abf7b1a0713de2aca
Gabriel Gomes <143620913+GOMESX7@users.noreply.github.com>
2024-09-16 11:11:14 -0300

    Update Aplicacao.java

--- src/br/com/gomesx7/cm/visao/Aplicacao.java
+++ src/br/com/gomesx7/cm/visao/Aplicacao.java
@@ -6,7 +6,7 @@ public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 0);
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
 		new TabuleiroConsole(tabuleiro);
   }
 
